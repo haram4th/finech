385a9dfe1afe9255b902e6e1be712197aa37283b
haram4th <haram4th@gmail.com>
2025-03-26 18:17:48 +0900

    함수_lambda함수

---
+++ 03python/함수.py
@@ -0,0 +1,16 @@
+
+def hello2():
+    print("hello2 파이썬 함수 만들기")
+
+hello2()
+
+def add(a, b):
+    return a + b
+
+print(add(10, 3))
+
+def circle(c):
+    print(f"반지름의 길이가 {c}인 원의 넓이는: {c*c*3.14} 입니다.")
+
+print(circle(5))
+
